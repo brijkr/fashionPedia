da84a1fc69445e1bbc8cf923d03886e71c18892d
brijendra kumar <imbrij@gmail.com>
2022-03-07 18:25:26 +0530

    Update load_images.py

--- load_images.py
+++ load_images.py
@@ -1,6 +1,8 @@
+import cv2
+import tensorflow as tf
 images = []
- for i in range(1,1001):
-     url = './clothing-co-parsing/photos/%04d.jpg'%(i)
+ for i in range(500):
+     url = './data/train/%04d.jpg'%(i)
      img = cv2.imread(url)
      img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
      images.append(tf.convert_to_tensor(img)) 
